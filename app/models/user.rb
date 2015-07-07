4da27f8021a906e6df11a28b37d4eb4c87356183
Kazunori Jo <itochan315@gmail.com>
2015-07-07 23:27:48 +0900

    Show Twitter icons

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ActiveRecord::Base
   has_one :comment
 
   def self.create_with_omniauth(auth)
-    create! do |user|
-      user.provider = auth['provider']
-      user.uid = auth['uid']
-      user.screen_name = auth['info']['nickname']
-      user.name = auth['info']['name']
-    end
+    user = find_or_initialize_by(provider: auth['provider'], uid: auth['uid'])
+    user.screen_name = auth['info']['nickname']
+    user.name = auth['info']['name']
+    user.profile_image_url = auth['info']['image']
+    user.save!
+
+    user
   end
 end
